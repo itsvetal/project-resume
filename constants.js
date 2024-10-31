9ba4f254d35e56488405d78449d094663739b374
iTsVetal <vitaliikryskiv.development@gmail.com>
2024-10-31 11:35:23 +0200

    Add files from branch dev V1.1.7

---
+++ constants.js
@@ -0,0 +1,24 @@
+export const softSkills = [
+    'Critical Thinking',
+    'Problem solving',
+    'Time management',
+    'Good communication'
+]
+
+export const hardSkills = [
+    'Basics of Java',
+    'OOP',
+    'Algorithms',
+    'Data Structure',
+    'GitHub',
+    'HTML',
+    'CSS, SASS',
+    'Bootstrap',
+    'Tailwind',
+    'REST Api',
+    'BEM',
+    'JavaScript',
+    'TypeScript,',
+    'React',
+    'Redux Toolkit'
+]
